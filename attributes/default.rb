da39a606f1bf41d3657130ba9be5f470c996a3f5
Swatniki, Jason <Jason.Swatniki@nordstrom.com>
2016-10-16 18:02:33 -0700

    Added apt-get update resource

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,8 @@
 # Attribute:: default
 #
 default['home_vbox_server']['apt_sources'] = [
-  'deb http://download.virtualbox.org/virtualbox/debian xenial contrib' ]
+  'deb http://download.virtualbox.org/virtualbox/debian xenial contrib',
+  'deb http://download.virtualbox.org/virtualbox/debian trusty contrib' ]
 default['home_vbox_server']['pkgs'] = [
   'build-essential',
   'virtualbox',
